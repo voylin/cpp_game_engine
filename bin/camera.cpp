991ef250767f2c5355aff6874efaf86d27ad15f1
voylin <0voylin0@gmail.com>
2022-12-26 08:33:23 +0900

    camera movement fix

--- bin/camera.cpp
+++ bin/camera.cpp
@@ -4,10 +4,18 @@
 namespace mke {
   void moveCamera(Camera &camera, CameraMovement movement) {
     switch (movement) {
-      case CameraMovement::MoveUp:    camera.position.z -= 0.02f;
-      case CameraMovement::MoveDown:  camera.position.z += 0.02f;
-      case CameraMovement::MoveLeft:  camera.position.x -= 0.02f;
-      case CameraMovement::MoveRight: camera.position.x += 0.02f;
+      case CameraMovement::MoveUp:
+        camera.position.z -= 0.02f;
+        break;
+      case CameraMovement::MoveDown:  
+        camera.position.z += 0.02f;
+        break;
+      case CameraMovement::MoveLeft:  
+        camera.position.x -= 0.02f;
+        break;
+      case CameraMovement::MoveRight: 
+        camera.position.x += 0.02f;
+        break;
       default: printError("Incorrect camera movement!");
     }
   }
